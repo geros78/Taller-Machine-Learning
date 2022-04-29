f1072485935dbe1e31566b1a6dbfd3f0bc1bdb0a
Alejandro Otaiza Orellano <alejo.o.o1000@gmail.com>
2022-04-28 19:22:53 -0500

    Agregada la base de datos

---
+++ BankData.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 18:25:46 2022
+
+@author: alejo
+"""
+import pandas as pd
+
+# Bank data
+
+url = './BankMarketing/bank-full.csv'
+data = pd.read_csv(url)
+
+# Limpiar la data
+
